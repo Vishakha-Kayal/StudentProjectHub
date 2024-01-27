bcc4726bb6ae702e90cae80e659d6a94a184e103
Anshika Malpani <138215307+Anshika-Malpani@users.noreply.github.com>
2024-01-27 16:01:53 +0530

    added footer section ,changes in index.ejs and script.js

--- StudentProjectHub/public/javascripts/script.js
+++ StudentProjectHub/public/javascripts/script.js
@@ -1,5 +1,5 @@
-gsap.to(".nav",{
-    backgroundColor:`rgb(203 213 225 /1)`,
+gsap.to(".nav", {
+    backgroundColor: `rgb(203 213 225 /1)`,
     // // backgroundColor:`cyan`,
 
     backdropFilter: "blur(10px)",
@@ -7,57 +7,72 @@ gsap.to(".nav",{
         trigger: ".nav",
         scroller: "body",
         // markers:true,
-        start:"top -1%",
-        end:"top -50%",
-        scrub:1
-    }     
-})
-
-gsap.from(".head",{
-    y:100,
-    duration:1,
-    opacity:0,
-    scrollTrigger:{
-        trigger:".head",
-        scroller:"body",
-        // markers:true,
-        start:"top 90%",
-        end:"top 70%",
-        scrub:2
+        start: "top -1%",
+        end: "top -50%",
+        scrub: 1
     }
 })
 
-function marquee(){
-    const anim=document.querySelectorAll(".anim")
+gsap.from(".head", {
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".head",
+        scroller: "body",
+        // markers:true,
+        start: "top 90%",
+        end: "top 70%",
+        scrub: 2
+    }
+})
+
+gsap.to(".nav", {
+    dispaly:"none",
+    opacity:0,
+    scrollTrigger: {
+        trigger: ".nav",
+        scroller: "body",
+        markers:true,
+        start: "top -250%",
+        end: "top -250%",
+        scrub: 1
+    }
+})
+
+
+
+function marquee() {
+    const anim = document.querySelectorAll(".anim")
     gsap.registerPlugin(ScrollTrigger);
-    
-    
-    anim.forEach(function(e){
+
+
+    anim.forEach(function (e) {
         gsap.set(e, { x: '0%' });
         gsap.to(e, {
-              x:"-100%", 
-              duration: 11, 
-              ease: 'linear',
-              repeat: -1, 
-              scrollTrigger:{
-            trigger:e,
-            scroller:"body",
-            // markers:true,
-            start:"top 80%",
-            end:"top 70%",
-            // scrub:true
-              }
-            },
-            
-          );
+            x: "-100%",
+            duration: 11,
+            ease: 'linear',
+            repeat: -1,
+            scrollTrigger: {
+                trigger: e,
+                scroller: "body",
+                // markers:true,
+                start: "top 80%",
+                end: "top 70%",
+                // scrub:true
+            }
+        },
+
+        );
     })
-} 
+}
 marquee();
 
 
+function typeWriterEffect() {
 
-
-const span = document.querySelector(".change");
+    const span = document.querySelector(".change");
 
     function typeWriter(textElement, text, speed, callback) {
         let i = 0;
@@ -67,7 +82,7 @@ const span = document.querySelector(".change");
                 i++;
                 setTimeout(type, speed);
             } else {
-                setTimeout(function(){
+                setTimeout(function () {
                     textElement.innerHTML = "";
                     if (callback) {
                         callback();
@@ -79,11 +94,13 @@ const span = document.querySelector(".change");
     }
 
     function changing() {
-        typeWriter(span, 'Project', 300, function() {
-            setTimeout(function(){
-                typeWriter(span, 'Talent', 300,changing);
+        typeWriter(span, 'Project', 300, function () {
+            setTimeout(function () {
+                typeWriter(span, 'Talent', 300, changing);
             }, 1000);
         });
     }
 
     changing();
+}
+typeWriterEffect();
