9f6d913e4064ff15cbd619b5b6082481febf196e
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-02-07 23:21:06 +0530

    js added

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -1,34 +1,35 @@
 let imgAttribute = document.querySelectorAll(".image");
 let imgdiv = document.querySelector(".project-im");
+console.log(typeof (imgdiv));
+console.log(Object.keys(imgdiv));
 let arr = Array.from(imgAttribute);
 leftBtn = document.querySelector(".left-icon");
 rightBtn = document.querySelector(".right-icon");
-// console.log(arr[0].getAttribute('src'));
-// let current = 0;
 
-
-let current = 0;
 let i = 0;
-
-
-
 rightBtn.addEventListener("click", () => {
-    console.log(arr[0]);
-    if (current == i) {
-        imgdiv.src = arr[current + 1].getAttribute('src');
-    }
-    current++;
     i++;
+    if (i > 3) {
+        i = 0;
+    }
+    imgdiv.src = arr[i].getAttribute('src');
 })
 
 leftBtn.addEventListener("click", () => {
-    console.log(arr[0]);
-    current--;
     i--;
-
-    if (current == i) {
-        imgdiv.src = arr[current].getAttribute('src');
-        // console.log(current);
-        // console.log(arr[current]);
+    if (i < 0) {
+        i = 3;
     }
+
+    imgdiv.src = arr[i].getAttribute('src');
+
 })
+
+
+
+
+// rightBtn.addEventListener("click", () => {
+//     current = (current + 1) % arr.length; // Cycle through images
+
+//     imgdiv.src = arr[current].getAttribute('src'); // Set the src attribute of imgdiv to the next image's src
+// });
