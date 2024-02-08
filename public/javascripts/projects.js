cfe494bd05d7b774baf635424957045f7c1217a9
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-02-08 23:53:52 +0530

    chatgpt js code

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -1,30 +1,61 @@
-let imgAttribute = document.querySelectorAll(".image");
-let imgdiv = document.querySelector(".project-im");
-console.log(typeof (imgdiv));
-console.log(Object.keys(imgdiv));
-let arr = Array.from(imgAttribute);
-leftBtn = document.querySelector(".left-icon");
-rightBtn = document.querySelector(".right-icon");
+// let imgAttribute = document.querySelectorAll(".image");
+// // let imgdiv = document.querySelector(".project-im");
+// let arr = Array.from(imgAttribute);
+// let arr2 = arr.slice();
+// leftBtn = document.querySelector(".left-icon");
+// rightBtn = document.querySelector(".right-icon");
 
-let i = 0;
+// let i = 0;
+// rightBtn.addEventListener("click", () => {
+//     i++;
+//     if (i > 3) {
+//         i = 0;
+//     }
+//     arr[0].src = arr2[i].getAttribute('src');
+// })
+
+// leftBtn.addEventListener("click", () => {
+//     i--;
+//     if (i < 0) {
+//         i = 3;
+//     }
+
+//     arr[0].src = arr2[i].getAttribute('src');
+//     console.log(arr[0].src);
+//     console.log(arr2[0].src);
+
+// })
+
+let imgAttribute = document.querySelectorAll(".image");
+let arr = Array.from(imgAttribute);
+let arrLength = arr.length;
+// console.log(1%4);
+let currentIndex = 0;
+let leftBtn = document.querySelector(".left-icon");
+let rightBtn = document.querySelector(".right-icon");
+
 rightBtn.addEventListener("click", () => {
-    i++;
-    if (i > 3) {
-        i = 0;
-    }
-    imgdiv.src = arr[i].getAttribute('src');
-})
+    currentIndex = (currentIndex + 1) % arrLength;
+    console.log(typeof((currentIndex + 1) % arrLength));
+    console.log(currentIndex);
+    updateImages();
+});
 
 leftBtn.addEventListener("click", () => {
-    i--;
-    if (i < 0) {
-        i = 3;
+    currentIndex = (currentIndex - 1 + arrLength) % arrLength;
+
+    updateImages();
+});
+
+function updateImages() {
+    for (let i = 0; i < arrLength; i++) {
+        if (i === currentIndex) {
+            arr[i].style.display = "block";
+        } else {
+            arr[i].style.display = "none";
+        }
     }
-
-    imgdiv.src = arr[i].getAttribute('src');
-
-})
-
+}
 
 
 
@@ -33,3 +64,4 @@ leftBtn.addEventListener("click", () => {
 
 //     imgdiv.src = arr[current].getAttribute('src'); // Set the src attribute of imgdiv to the next image's src
 // });
+// console.log(Object.keys(imgdiv));
