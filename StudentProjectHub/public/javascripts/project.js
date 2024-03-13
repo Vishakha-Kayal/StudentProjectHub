3c3ee870ebffa546e20a82c45f706709ecb6ac43
Anshika Malpani <138215307+Anshika-Malpani@users.noreply.github.com>
2024-03-13 18:36:35 +0530

    changes in project.ejs

--- StudentProjectHub/public/javascripts/project.js
+++ StudentProjectHub/public/javascripts/project.js
@@ -2,52 +2,55 @@ let imgAttribute = document.querySelectorAll(".image");
 let imgdiv = document.querySelector(".actual");
 let arr = Array.from(imgAttribute);
 
-leftBtn = document.querySelector(".left-icon");
-rightBtn = document.querySelector(".right-icon");
+const leftBtn = document.querySelector(".left-icon");
+const rightBtn = document.querySelector(".right-icon");
 
 let i = 0;
+const totalImages = arr.length; 
+
 rightBtn.addEventListener("click", () => {
-    i++;
-    if (i > 3) {
-        i = 0;
-    }
+    i = (i + 1) % totalImages; 
     imgdiv.src = arr[i].getAttribute('src');
-})
+});
 
 leftBtn.addEventListener("click", () => {
-    i--;
-    if (i < 0) {
-        i = 3;
-    }
-
+    i = (i - 1 + totalImages) % totalImages; 
     imgdiv.src = arr[i].getAttribute('src');
-    console.log(arr[0].src);
-    console.log(arr[0].src);
-
-})
+});
 
 
 const dropdown = document.querySelector(".dropdown");
 const dropdownScnd = document.querySelector(".dropdown-scnd");
 const filters = document.querySelectorAll(".filters");
 
-filters[1].addEventListener("click",()=>{
-    if(!dropdownScnd.classList.contains("hidden")){
-        dropdownScnd.classList.toggle("hidden");
-        dropdown.classList.toggle("hidden");
+
+function toggleDropdown(dropdownToHide, dropdownToShow, filter) {
+    if (!dropdownToShow.classList.contains("hidden")) {
+        dropdownToShow.classList.add("hidden");
     }
-   else{
-    dropdown.classList.toggle("hidden");
-   }
-    
-})
-filters[4].addEventListener("click",()=>{
-    if(!dropdown.classList.contains("hidden")){
-        dropdown.classList.toggle("hidden");
-        dropdownScnd.classList.toggle("hidden");
+    dropdownToHide.classList.toggle("hidden");
+    updateFilterStyle(filter, dropdownToHide.classList.contains("hidden"));
+}
+
+
+function updateFilterStyle(filter, isHidden) {
+    if (isHidden) {
+        filter.classList.add("text-white", "bg-[#43856F]");
+        filter.classList.remove("border-black");
+    } else {
+        filter.classList.remove("text-white", "bg-[#43856F]");
+        filter.classList.add("border-black");
     }
-   else{
-    dropdownScnd.classList.toggle("hidden");
-}})
+}
+
+filters[1].addEventListener("click", () => {
+    toggleDropdown(dropdown, dropdownScnd, filters[1]);
+});
+
+filters[4].addEventListener("click", () => {
+    toggleDropdown(dropdownScnd, dropdown, filters[4]);
+});
+
+
 
 
