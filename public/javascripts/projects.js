b08928d1806348953b7343652631ff82b9602ebe
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-03-13 18:34:38 +0530

    added functionality to drop down

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -25,3 +25,50 @@ leftBtn.addEventListener("click", () => {
     console.log(arr[0].src);
 
 })
+
+
+const dropdown = document.querySelector(".dropdown");
+const dropdownScnd = document.querySelector(".dropdown-scnd");
+const filters = document.querySelectorAll(".filters");
+
+filters[1].addEventListener("click", () => {
+    if (!dropdownScnd.classList.contains("hidden")) {
+        dropdownScnd.classList.toggle("hidden");
+        dropdown.classList.toggle("hidden");
+    }
+    else {
+        dropdown.classList.toggle("hidden");
+    }
+
+})
+filters[4].addEventListener("click", () => {
+    if (!dropdown.classList.contains("hidden")) {
+        dropdown.classList.toggle("hidden");
+        dropdownScnd.classList.toggle("hidden");
+    }
+    else {
+        dropdownScnd.classList.toggle("hidden");
+    }
+})
+
+const icon = document.querySelectorAll('.ic')
+const universityname = document.querySelector('.uname')
+const selectedCollege = document.querySelectorAll('.checkbox div')
+const updatedUniversityNmae = document.querySelector('.updated-uni-name')
+const closeicon = document.createElement('i')
+
+selectedCollege.forEach((collegeElement) => {
+    collegeElement.addEventListener('click', (event) => {
+        universityname.remove();
+        icon[0].remove();
+        icon[1].remove();
+        updatedUniversityNmae.textContent = event.currentTarget.textContent;
+        updatedUniversityNmae.classList.add('uni-style')
+        closeicon.innerHTML = `<i class="ri-close-circle-line"></i>`
+        filters[1].append(closeicon)
+        filters[1].style.backgroundColor = "#e4ebe8";
+        filters[1].style.color = 'black';
+        filters[1].style.border = '1px solid black';
+
+    });
+});
